5ad30ba4d1636f5d4a09539f91850e337fe99ed2
AndrewKorchemniy <andrewkorch@gmail.com>
2023-05-09 15:01:45 -0700

    Conditional rendering of requirements button

--- studentpage/src/components/molecules/navbar.rs
+++ studentpage/src/components/molecules/navbar.rs
@@ -1,12 +1,15 @@
 use crate::components::atoms::button::{Button, ButtonVariant};
 use yew::prelude::*;
-use yew_router::prelude::*;
+use yew_router::{prelude::*};
 
 use crate::Route;
 
 /// The [Navbar] component provides a styled navbar for the application.
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
+    let route: Option<Route> = use_route::<Route>();
+    let is_route_home = || matches!(route.unwrap(), Route::Home);
+
     html! {
         <nav class="navbar rounded ps-3 pe-2 py-1 shadow-sm my-3 font-weight-bold">
             <span class="navbar-link">
@@ -16,9 +19,11 @@ pub fn navbar() -> Html {
                 </Link<Route>>
             </span>
             <span>
-                <Link<Route> to={ Route::Requirements }>
-                    <Button variant={ ButtonVariant::Primary } class="me-2" label="Requirements" />
-                </Link<Route>>
+                if is_route_home() {
+                    <Link<Route> to={ Route::Requirements }>
+                        <Button variant={ ButtonVariant::Primary } class="me-2" label="Requirements" />
+                    </Link<Route>>
+                }
                 <Button variant={ ButtonVariant::Warning } label="Logout" />
             </span>
         </nav>
