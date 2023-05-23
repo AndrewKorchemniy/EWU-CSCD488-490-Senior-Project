41063b78acbca7fb9d7b0483a2eaae746df5c8a9
Timbre Freeman <timbref@gmail.com>
2023-05-23 10:27:10 -0700

    helper script

---
+++ helper_scripts/dev_run_on_aws.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# color variables
+Color_Off='\033[0m'       # Text Reset
+Cyan='\033[0;36m'         # Cyan
+Purple='\033[0;35m'       # Purple
+
+cd /home/tztz8/dev/EWU-CSCD488-490-Senior-Project
+
+echo -e "$Cyan Build... $Color_Off"
+cd backend
+cargo build --release
+cd ..
+cd adminpage
+trunk build --release
+cd ..
+cd studentpage
+trunk build --release
+cd ..
+echo -e "$Cyan Stop old server... $Color_Off"
+ssh ec2-user@aws.tftinker.tech 'sudo systemctl stop actix'
+ssh ec2-user@aws.tftinker.tech 'kill $(lsof -ti:8443)'
+
+echo -e "$Cyan Copy new server... $Color_Off"
+# backend
+scp target/release/backend ec2-user@aws.tftinker.tech:/home/ec2-user/server
+scp server.config.toml ec2-user@aws.tftinker.tech:/home/ec2-user/server
+# pages
+ssh ec2-user@aws.tftinker.tech 'mv /home/ec2-user/server/studentpage /home/ec2-user/server/studentpage-old'
+ssh ec2-user@aws.tftinker.tech 'mkdir -p /home/ec2-user/server/studentpage'
+scp -r studentpage/dist ec2-user@aws.tftinker.tech:/home/ec2-user/server/studentpage/dist
+ssh ec2-user@aws.tftinker.tech 'mv /home/ec2-user/server/adminpage /home/ec2-user/server/adminpage-old'
+ssh ec2-user@aws.tftinker.tech 'mkdir -p /home/ec2-user/server/adminpage'
+scp -r adminpage/dist ec2-user@aws.tftinker.tech:/home/ec2-user/server/adminpage/dist
+# res folder
+ssh ec2-user@aws.tftinker.tech 'mv /home/ec2-user/server/res /home/ec2-user/server/res-old'
+scp -r adminpage/dist/* ec2-user@aws.tftinker.tech:/home/ec2-user/server/res
+scp -r studentpage/dist/* ec2-user@aws.tftinker.tech:/home/ec2-user/server/res
+ssh ec2-user@aws.tftinker.tech 'rm /home/ec2-user/server/res/index.html'
+
+echo -e "$Cyan SSH to start new server... $Color_Off"
+echo -e "$Purple run 'cd /home/ec2-user/server && ./backend'... $Color_Off"
+ssh ec2-user@aws.tftinker.tech
+#ssh ec2-user@aws.tftinker.tech '/usr/bin/sh /home/ec2-user/server/run.sh'
+#ssh ec2-user@aws.tftinker.tech 'sudo systemctl start actix'
+#ssh ec2-user@aws.tftinker.tech 'sudo systemctl status actix'
+echo -e "$Purple Done... $Color_Off"
