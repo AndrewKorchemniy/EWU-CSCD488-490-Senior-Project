9e3e41c21373d99ed399af4d3ef411f59e1475a1
Wvvern <andrewkorch@gmail.com>
2023-05-28 20:48:59 -0700

    Add spinner

--- studentpage/src/lib.rs
+++ studentpage/src/lib.rs
@@ -7,6 +7,8 @@ use yew_router::prelude::*;
 
 mod components;
 use components::footer::Footer;
+// TODO: uncomment import when the API for OAuth is ready.
+//use components::msgbox::{MsgBox, MsgBoxVariant};
 use components::navbar::Navbar;
 
 mod pages;
@@ -60,7 +62,6 @@ pub fn app_main() -> Html {
             </div>
         </BrowserRouter>
         // TODO: Replace the above with the following when the API for OAuth is ready.
-        // use components::msgbox::{MsgBox, MsgBoxVariant};
         // <BrowserRouter>
         //     <div class="container-fluid mx-auto px-4 min-width">
         //         <Failure>
@@ -95,15 +96,17 @@ const STYLESHEET: &str = include_str!("assets/main.css");
 
 #[function_component(App)]
 pub fn app() -> Html {
-    // Set up global stylesheet
+    // Set up the scoped stylesheet (global)
     let stylesheet = Style::new(STYLESHEET).unwrap();
 
-    // The state for the OAuth2 config
+    // The state of the oauth config request
     let _config_request_state = use_state(|| false as bool);
     let config_request_state = _config_request_state.clone();
     let config_request_state_changes = Callback::from(move |value: bool| {
         _config_request_state.set(value);
     });
+
+    // The state of the oauth config
     let _config_state = use_state(|| (String::new(), String::new(), String::new()));
     let config_state = _config_state.clone();
     let config_state_changes = Callback::from(move |config: OAuthClientConfigResponse| {
@@ -111,6 +114,7 @@ pub fn app() -> Html {
         config_request_state_changes.emit(true);
     });
 
+    // Fetch the oauth config if it hasn't been fetched yet
     if !*config_request_state {
         wasm_bindgen_futures::spawn_local(async move {
             let result = api_get_auth_config().await;
@@ -119,8 +123,8 @@ pub fn app() -> Html {
     }
 
     html! {
-        if *config_request_state {
-            <div class={stylesheet}>
+        <div class={stylesheet}>
+            if *config_request_state {
                 <OAuth2 config={{
                     let (client_id, auth_url, token_url) = &*config_state;
                     Config {
@@ -130,8 +134,14 @@ pub fn app() -> Html {
                     }}} >
                     <AppMain />
                 </OAuth2>
-            </div>
-        }
+            } else {
+                <div class="spinner-wrapper text-primary">
+                    <div class="spinner-grow" role="status" style="width: 6rem; height: 6rem;">
+                        <span class="visually-hidden"> {"Loading..."} </span>
+                    </div>
+                </div>
+            }
+        </div>
     }
 }
 
