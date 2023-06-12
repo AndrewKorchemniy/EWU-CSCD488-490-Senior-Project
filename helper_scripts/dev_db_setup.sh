9523254263386c4d70876a31c619ce3969f02a1b
TheHateofTheFox <ngainer@ewu.edu>
2023-06-12 11:44:56 -0700

    updated some db scripts

--- helper_scripts/dev_db_setup.sh
+++ helper_scripts/dev_db_setup.sh
@@ -1,3 +1,4 @@
+
 ./target/release/dbcli user create 'student@ewu.edu' '123456' 'stu' 'steiner' 'eagles'
 
 ./target/release/dbcli sprint create '1' '2023-06-02'
@@ -11,6 +12,27 @@
 ./target/release/dbcli individual create 'student@ewu.edu' '1'
 
 
+./target/release/dbcli user create 'student2@ewu.edu' '123456' 'bob' 'builder' 'cats'
+
+./target/release/dbcli sprint create '3' '2023-06-02'
+
+./target/release/dbcli team-activity create 'cats' 'student2@ewu.edu' '3'
+
+./target/release/dbcli team-report create 'cats' '3'
+
+./target/release/dbcli requirements create 'cats' '3'
+
+./target/release/dbcli individual create 'student2@ewu.edu' '3'
 
 
+./target/release/dbcli user create 'student3@ewu.edu' '123456' 'joe' 'smith' 'dogs'
 
+./target/release/dbcli sprint create '2' '2023-06-02'
+
+./target/release/dbcli team-activity create 'dogs' 'student3@ewu.edu' '2'
+
+./target/release/dbcli team-report create 'dogs' '2'
+
+./target/release/dbcli requirements create 'dogs' '2'
+
+./target/release/dbcli individual create 'student3@ewu.edu' '2'
